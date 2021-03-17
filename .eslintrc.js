e6140619a574c61fcaffdf73b29169b32eb70e89
Diego Prieto <diego.prieto@pager.com>
2021-03-17 15:29:43 -0500

    fix: eslint ecma version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
+'use strict';
+
 module.exports = {
-  extends: 'hapi',
-  parserOptions: {
-    ecmaVersion: 9
-  },
-  rules: {},
-  globals: {
-    describe: true,
-    it: true,
-    beforeEach: true,
-    afterEach: true
-  }
-}
+    extends: 'hapi',
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {},
+    globals: {
+        describe: true,
+        it: true,
+        beforeEach: true,
+        afterEach: true
+    }
+};
